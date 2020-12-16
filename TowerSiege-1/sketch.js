0abcea3252f99b058e58b6fe95d8cfce7c7e2a23
Anandini-mehta <72254851+Anandini-mehta@users.noreply.github.com>
2020-12-16 20:04:45 +0530

    Add files via upload

---
+++ TowerSiege-1/sketch.js
@@ -0,0 +1,28 @@
+setup() {
+
+// level 1 
+block1 = new block(300,155,30,40);  
+
+//level 2 
+block1 = new block(300,235,30,40);
+block1 = new block(300,235,30,40);
+block1 = new block(300,235,30,40);
+block1 = new block(300,235,30,40); 
+block1 = new block(300,235,30,40);
+
+//level 3
+block1 = new block(330,195,30,40);
+block1 = new block(360,195,30,40);
+block1 = new block(390,195,30,40);  
+
+polygon = Bodies.circle(50,200,20);
+world.add(world,polygon); 
+
+SlingShot = new Slingshot(this.polygon{x:100,y:200}) 
+
+}
+
+
+
+
+
